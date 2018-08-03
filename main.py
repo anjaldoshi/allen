0e9e5322e9cebf5f9605f321b0040044dbb6cc45
Anjal <anjaldoshi09@gmail.com>
2018-08-03 13:28:45 -0700

    Dynamic Feature Selection

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
-import sys, numpy, json, h5py, random
+import sys, numpy, json, h5py, h5py_cache, random
 from PIL import Image
 
 totalImages = 999
-sw = sh = width = height = textures = maskTextures =  currImage = iNext = iPrev = zoom1 = zoom2 = fData1 = fData2 = fwd = rvs = MouseX = MouseY = hover = frame_count = start_time = end_time = fps = 0
+sw = sh = width = height = textures = maskTextures = maskZoom = currImage = iNext = iPrev = zoom1 = zoom2 = fwd = rvs = MouseX = MouseY = hover = frame_count = start_time = end_time = fps = maskZoomCount = 0
 traceStart = 1
 traceEnd = 1000
 zoomFactor = 1.0
@@ -17,9 +17,10 @@ featureSize = []
 traces = []
 masks = []
 randRGB = []
+maskSelected = [0,0,0]
 
 def init():
-    global image, bufferImage, width, height, textures, maskTextures, traces, featureOffset, featureSize, masks, randRGB
+    global image, bufferImage, width, height, textures, maskTextures, maskZoom, traces, featureOffset, featureSize, masks, randRGB
     glClearColor(0.0,0.0,0.0,0.0)
 
     hf  = h5py.File('mask.h5', 'r')
@@ -35,9 +36,9 @@ def init():
 
     hf2 = h5py.File('roi_traces.h5', 'r')
     traces = hf2.get('data')
-    traces = traces[1,:]
+    traces = traces[:,:]
 
-    hf3 = h5py.File('motion_corrected_video.h5', 'r')
+    hf3 = h5py_cache.File('motion_corrected_video.h5', 'r', chunk_cache_mem_size=1024**2*2000)
     bufferImage = hf3.get('data')
     image = bufferImage[0,:,:]
     image = image.transpose(1,0)
@@ -56,7 +57,7 @@ def init():
         #featureOffset.append((numpy.random.randint(low=50, high=900), (numpy.random.randint(low=50, high=800))))
     for data in range(len(masks)):    
         featureOffset.append((offX[data]*(sw/(2*width)), offY[data]*(sw/(2*width))))
-        featureSize.append((fsizeX[data], fsizeY[data]))
+        featureSize.append((fsizeX[data]*(sw/(2*width)), fsizeY[data]*(sw/(2*width))))
         r = random.uniform(0, 0.2)
         g = random.uniform(0, 0.2)
         b = random.uniform(0, 0.2)
@@ -80,13 +81,16 @@ def init():
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
     glTexImage2D(GL_TEXTURE_2D, 0, GL_LUMINANCE, width, height, 0, GL_LUMINANCE, GL_SHORT, image)
 
-    glBindTexture(GL_TEXTURE_2D, textures[2])
-    glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_LUMINANCE, width, height, 0, GL_LUMINANCE, GL_SHORT, image)
+
+    maskZoom = glGenTextures(3)
+    for i in range(3):
+        glBindTexture(GL_TEXTURE_2D, maskZoom[i])
+        glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_LUMINANCE, len(masks[i][0]), len(masks[i]), 0, GL_LUMINANCE, GL_UNSIGNED_BYTE, masks[i])
 
     # glGenBuffers(1, pbo)
     # glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pbo)
@@ -207,7 +211,7 @@ def drawOverlay(feature):
 #     glEnd()
 
 
-def featureZoom():
+def hoverZoom():
     glViewport(int(sw/4), int(sh-sw/4.8), int(sw/5), int(sw/5))
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
@@ -235,27 +239,22 @@ def featureZoom():
     
     glColor3ub(255, 255, 255)
     glRasterPos2f(20, 20)
-    s = "Feature Zoom 1:"
+    s = "Hover to Zoom:"
     for ch in s:
         glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, ord(ch))
 
-    if zoom1 == 1:
-        glBegin(GL_LINE_LOOP)
-        glVertex2f(0.1, 0.1)
-        glVertex2f(0.1, sw/5-0.1)
-        glVertex2f(sw/5, sw/5-0.1)
-        glVertex2f(sw/5, 0.1)
-        glEnd()
-
     glPopMatrix()
 
-    glViewport(int(sw/4), int(sh-sw/2.4), int(sw/5), int(sw/5))
+
+
+def displayMaskZoom(num):
+    glViewport(int(sw/4), int(sh-(sw/4.6) - len(masks[num])*(3*(num+1)) - (15*num)), int(len(masks[num][0]))*3, int(len(masks[num]))*3)
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
-    gluOrtho2D(0, int(sw/5), int(sw/5), 0)
+    gluOrtho2D(0, int(len(masks[num][0]))*3, int(len(masks[num]))*3, 0)
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
-    glBindTexture(GL_TEXTURE_2D, textures[2])
+    glBindTexture(GL_TEXTURE_2D, maskZoom[num])
     glEnable(GL_TEXTURE_2D)
     glPushMatrix()
     glClear(GL_DEPTH_BUFFER_BIT)
@@ -263,28 +262,50 @@ def featureZoom():
     glTexCoord2f(0.0, 0.0)
     glVertex3f(0.0, 0.0, 0.0)
     glTexCoord2f(0.0, 1.0)
-    glVertex3f(0.0, sw/5, 0.0)
+    glVertex3f(0.0, len(masks[num])*3, 0.0)
     glTexCoord2f(1.0, 1.0)
-    glVertex3f(sw/5, sw/5, 0.0)
+    glVertex3f(len(masks[num][0])*3, len(masks[num])*3, 0.0)
     glTexCoord2f(1.0, 0.0)
-    glVertex3f(sw/5, 0.0, 0.0)
+    glVertex3f(len(masks[num][0])*3, 0.0, 0.0)
     glEnd()
     glDisable(GL_TEXTURE_2D)
-    
-    glColor3ub(255, 255, 255)
-    glRasterPos2f(20, 20)
-    s = "Feature Zoom 2:"
-    for ch in s:
-        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, ord(ch))
 
-    if zoom2 == 1:
-        glBegin(GL_LINE_LOOP)
-        glVertex2f(0.1, 0.1)
-        glVertex2f(0.1, sw/5-0.1)
-        glVertex2f(sw/5, sw/5-0.1)
-        glVertex2f(sw/5, 0.1)
-        glEnd()
+    glBegin(GL_LINE_LOOP)
+    glVertex2f(0.1, 0.1)
+    glVertex2f(0.1, len(masks[num])*3-0.1)
+    glVertex2f(len(masks[num][0])*3, len(masks[num])*3-0.1)
+    glVertex2f(len(masks[num][0])*3, 0.1)
+    glEnd()
+
     glPopMatrix()
+
+
+def displayMaskDetails(num):
+    glViewport(30, int(sh-(sw/4.6) - len(masks[num])*(3*(num+1)) - (15*num)), int(sw/5), int(len(masks[num]))*3)
+    glLoadIdentity()
+    gluOrtho2D(0, int(sw/5), int(len(masks[num]))*3, 0)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+    glBegin(GL_LINE_LOOP)
+    glVertex2f(0.1, 0.1)
+    glVertex2f(0.1, len(masks[num])*3-0.1)
+    glVertex2f(sw/5, len(masks[num])*3-0.1)
+    glVertex2f(sw/5, 0.1)
+    glEnd()
+    glRasterPos2f(20, 20)
+    s1 = 'Feature selected: ' + str(maskSelected[num])
+    for ch in s1:
+        glutBitmapCharacter( GLUT_BITMAP_HELVETICA_18 , ord(ch))
+
+    glRasterPos2f(20, len(masks[num]))
+    s1 = 'Location: ' + str(featureOffset[maskSelected[num]][0]) + ', ' + str(featureOffset[maskSelected[num]][1])
+    for ch in s1:
+        glutBitmapCharacter( GLUT_BITMAP_HELVETICA_18 , ord(ch))
+    
+    glRasterPos2f(20, len(masks[num])*2)
+    s1 = "Area: " + str(featureSize[maskSelected[num]][0]) + ', ' + str(featureSize[maskSelected[num]][1])
+    for ch in s1:
+        glutBitmapCharacter( GLUT_BITMAP_HELVETICA_18 , ord(ch))
 
 
 def featureDetails():
@@ -302,12 +323,12 @@ def featureDetails():
     glVertex2f(sw/5, 0.1)
     glEnd()
     glRasterPos2f(20, (sw/5)/5)
-    s1 = 'Feature selected: ' + str(fData1)
+    s1 = 'Feature selected: ' + str(0)
     for ch in s1:
         glutBitmapCharacter( GLUT_BITMAP_HELVETICA_18 , ord(ch))
 
     glRasterPos2f(20, (sw/5)/3)
-    s1 = 'Location: ' + str(featureOffset[fData1][0]) + ', ' + str(featureOffset[fData1][1])
+    s1 = 'Location: ' + str(featureOffset[0][0]) + ', ' + str(featureOffset[0][1])
     for ch in s1:
         glutBitmapCharacter( GLUT_BITMAP_HELVETICA_18 , ord(ch))
     
@@ -321,57 +342,57 @@ def featureDetails():
     for ch in s1:
         glutBitmapCharacter( GLUT_BITMAP_HELVETICA_18 , ord(ch))
     
-    glViewport(30, int(sh-sw/2.4), int(sw/5), int(sw/5))
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    gluOrtho2D(0, int(sw/5), int(sw/5), 0)
-    glMatrixMode(GL_MODELVIEW)
-    glLoadIdentity()
-    glBegin(GL_LINE_LOOP)
-    glVertex2f(0.1, 0.1)
-    glVertex2f(0.1, sw/5-0.1)
-    glVertex2f(sw/5, sw/5-0.1)
-    glVertex2f(sw/5, 0.1)
-    glEnd()
-    glRasterPos2f(20, (sw/5)/5)
-    s1 = 'Feature selected: ' + str(fData2)
-    for ch in s1:
-        glutBitmapCharacter( GLUT_BITMAP_HELVETICA_18 , ord(ch))
+    # glViewport(30, int(sh-sw/2.4), int(sw/5), int(sw/5))
+    # glMatrixMode(GL_PROJECTION)
+    # glLoadIdentity()
+    # gluOrtho2D(0, int(sw/5), int(sw/5), 0)
+    # glMatrixMode(GL_MODELVIEW)
+    # glLoadIdentity()
+    # glBegin(GL_LINE_LOOP)
+    # glVertex2f(0.1, 0.1)
+    # glVertex2f(0.1, sw/5-0.1)
+    # glVertex2f(sw/5, sw/5-0.1)
+    # glVertex2f(sw/5, 0.1)
+    # glEnd()
+    # glRasterPos2f(20, (sw/5)/5)
+    # s1 = 'Feature selected: ' + str(0)
+    # for ch in s1:
+    #     glutBitmapCharacter( GLUT_BITMAP_HELVETICA_18 , ord(ch))
 
-    glRasterPos2f(20, (sw/5)/3)
-    s1 = 'Location: ' + str(featureOffset[fData2][0]) + ', ' + str(featureOffset[fData2][1])
-    for ch in s1:
-        glutBitmapCharacter( GLUT_BITMAP_HELVETICA_18 , ord(ch))
+    # glRasterPos2f(20, (sw/5)/3)
+    # s1 = 'Location: ' + str(featureOffset[0][0]) + ', ' + str(featureOffset[0][1])
+    # for ch in s1:
+    #     glutBitmapCharacter( GLUT_BITMAP_HELVETICA_18 , ord(ch))
     
-    glRasterPos2f(20, (sw/5)/2)
-    s1 = "Area: 1600px"
-    for ch in s1:
-        glutBitmapCharacter( GLUT_BITMAP_HELVETICA_18 , ord(ch))
+    # glRasterPos2f(20, (sw/5)/2)
+    # s1 = "Area: 1600px"
+    # for ch in s1:
+    #     glutBitmapCharacter( GLUT_BITMAP_HELVETICA_18 , ord(ch))
     
-    glRasterPos2f(20, (sw/5)/1.5)
-    s1 = "Perimeter: 160px"
-    for ch in s1:
-        glutBitmapCharacter( GLUT_BITMAP_HELVETICA_18 , ord(ch))
+    # glRasterPos2f(20, (sw/5)/1.5)
+    # s1 = "Perimeter: 160px"
+    # for ch in s1:
+    #     glutBitmapCharacter( GLUT_BITMAP_HELVETICA_18 , ord(ch))
 
 
-def plotTraces():
+def plotTraces(num):
     global traceStart, traceEnd
     glColor3ub(255, 255, 255)
-    glViewport(0, 100, sw, 100)
+    glViewport(0, int(sh-(sw/2)*(height/width)-(110*(num+1))), sw, 90)
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
-    gluOrtho2D(0, sw, 100, 0)
+    gluOrtho2D(0, sw, 0, 90)
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
     glBegin(GL_LINES)
-    glVertex2f(0, 0)
-    glVertex2f(sw, 0)
-    glVertex2f(0, 100-0.1)
-    glVertex2f(sw, 100-0.1)
+    glVertex2f(0, 0.1)
+    glVertex2f(sw, 0.1)
+    glVertex2f(0, 90-0.1)
+    glVertex2f(sw, 90-0.1)
     glEnd()
     glBegin(GL_LINES)
     glVertex2f(sw/2, 0)
-    glVertex2f(sw/2, 100)
+    glVertex2f(sw/2, 90)
     glEnd()
     if currImage < len(bufferImage)-400 and currImage >= 400:
         traceStart = currImage - 400
@@ -382,7 +403,8 @@ def plotTraces():
             traceEnd = 800
         elif currImage > len(bufferImage)-400:
             traceStart = currImage - 400
-            traceEnd = len(bufferImage)-1
+            traceEnd = len(bufferImage)
+    #glScalef(1.0, -1.0, 1.0)
     glBegin(GL_LINES)
     # if currImage > traceEnd - 400 and currImage < len(bufferImage)-400:
     #     traceEnd += 400
@@ -391,8 +413,8 @@ def plotTraces():
     #     traceStart -= 400
     #     traceEnd -= 400
     for point in range(traceStart, traceEnd):
-        glVertex2f((sw/2-currImage*5)+(point-1)*5, (traces[point-1])/50-60)
-        glVertex2f((sw/2-currImage*5)+point*5, (traces[point])/50-60)
+        glVertex2f((sw/2-currImage*5)+(point-1)*5, traces[maskSelected[num]][point-1]/250)
+        glVertex2f((sw/2-currImage*5)+point*5, traces[maskSelected[num]][point]/250)
     glEnd()
 
 
@@ -420,9 +442,12 @@ def display():
         glBlendColor(randRGB[feature][0], randRGB[feature][1], randRGB[feature][2], 1)
         drawOverlay(feature)
     glDisable(GL_BLEND)
-    featureZoom()
+    hoverZoom()
+    for i in range(maskZoomCount):
+        displayMaskZoom(i)
+        displayMaskDetails(i)
+        plotTraces(i)
     featureDetails()
-    plotTraces()
     playVideo()
     frame_count += 1
     end_time = glutGet(GLUT_ELAPSED_TIME)
@@ -436,24 +461,26 @@ def display():
 
 def Timer(value):
     glutPostRedisplay()
-    glutTimerFunc(1, Timer, 0)
+    glutTimerFunc(30, Timer, 0)
 
 
 def mouseAction(button, state, x, y):
-    global fData1, fData2
+    global maskZoomCount, maskSelected
     if state == GLUT_DOWN and button == GLUT_LEFT_BUTTON:
-        # if x < int((sw/2)+featureOffset[currImage][0]+featureSize[currImage][0]) and x > int((sw/2)+featureOffset[currImage][0]) and y < int(featureOffset[currImage][1]+featureSize[currImage][1]) and y > int(featureOffset[currImage][1]):
-        #     if zoom1 == 1:
-        #         glBindTexture(GL_TEXTURE_2D, textures[1])
-        #         glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height, GL_BGRA, GL_UNSIGNED_BYTE, image[currImage])
-        #         fData1 = currImage
-        #     elif zoom2 == 1:
-        #         glBindTexture(GL_TEXTURE_2D, textures[2])
-        #         glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height, GL_BGRA, GL_UNSIGNED_BYTE, image[currImage])
-        #         fData2 = currImage
         if x > int(sw/2) and y < int(sw/2*(height/width)):
-            glBindTexture(GL_TEXTURE_2D, textures[2])
-            glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, len(masks[10][0]), len(masks[10]), GL_LUMINANCE, GL_UNSIGNED_BYTE, masks[10])
+            for i in range(len(featureOffset)):
+                if x < int(sw/2 + featureOffset[i][0] + featureSize[i][0]) and x > int(sw/2 + featureOffset[i][0]) and y < int(featureOffset[i][1] + featureSize[i][1]) and y > featureOffset[i][1]:
+                    if(maskZoomCount < 3):
+                        maskSelected[maskZoomCount] = i
+                        glBindTexture(GL_TEXTURE_2D, maskZoom[maskZoomCount])
+                        glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, len(masks[i][0]), len(masks[i]), GL_LUMINANCE, GL_UNSIGNED_BYTE, masks[i])
+                        maskZoomCount += 1
+                    else:
+                        maskZoomCount = 2
+                        maskSelected[maskZoomCount] = i
+                        glBindTexture(GL_TEXTURE_2D, maskZoom[maskZoomCount])
+                        glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, len(masks[i][0]), len(masks[i]), GL_LUMINANCE, GL_UNSIGNED_BYTE, masks[i])
+                    break
 
     glutPostRedisplay()   
 
@@ -467,7 +494,7 @@ def zoomLocation(x, y):
         glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height, GL_LUMINANCE, GL_SHORT, image)
         zoomFactor = 7.5
     else:
-        featureImage = bufferImage[fData1,:,:]
+        featureImage = bufferImage[0,:,:]
         featureImage = featureImage.transpose(1,0)
         glBindTexture(GL_TEXTURE_2D, textures[1])
         glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height, GL_LUMINANCE, GL_SHORT, featureImage)
